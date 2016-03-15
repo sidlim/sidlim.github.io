3050f0eb5c06921658a82163456f2f62c085cd3c
S L <sid.limaye11@gmail.com>
2016-03-15 13:47:29 -0400

    Dynamic Menu pt 2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-/* Menu setup. */
+/* Onload stuff. */
+window.document.body.addEventListener('load', loader);
+    function loader() {
+        var menu = document.createElement('div');
+        document.body.appendChild(menu);
+        menu.id = 'menu';
+        var Mspacing = document.createElement('div');
+        menu.appendChild(Mspacing);
+        Mspacing.className = 'menuspacing';
+    }
