de06d202b9d6462be424d53e5eab6695bc21b99d
S L <sid.limaye11@gmail.com>
2016-11-19 20:45:51 -0800

    Started rewrite.

--- index.js
+++ index.js
@@ -1,48 +1,26 @@
-/* Onload stuff. */
-var data;
-window.addEventListener('load', loader);
-    function loader() {
-        var menu = document.createElement('div');
-        document.body.appendChild(menu);
-        menu.id = 'menu';
-        var Mspacing = document.createElement('div');
-        menu.appendChild(Mspacing);
-        Mspacing.className = 'menuspacing';
-        
-        request = new XMLHttpRequest();
-        request.open('GET', 'http://eyamil.github.io/posts.json', true);
-        
-        request.onload = function() {
-            if (request.status >= 200 && request.status < 400){
-                // Success!
-                data = JSON.parse(request.responseText);
-                for (var n = 0; n < Object.keys(data.Pages).length; n++) {
-                    var link = document.createElement('a');
-                    var menuitem = document.createElement('span')
-                    Mspacing.appendChild(link);
-                    link.appendChild(menuitem);
-                    menuitem.className = 'menulink';
-                    menuitem.innerText = Object.keys(data.Pages)[n];
-                    link.href = data.Pages[Object.keys(data.Pages)[n]].url;
-                };
-                for (var n = 0; n < Object.keys(data.Posts).length; n++) {
-                    var link = document.createElement('a');
-                    var menuitem = document.createElement('span')
-                    Mspacing.appendChild(link);
-                    link.appendChild(menuitem);
-                    menuitem.className = 'menulink';
-                    menuitem.innerText = Object.keys(data.Posts)[n];
-                    link.href = data.Posts[Object.keys(data.Posts)[n]].url;
-                };
-            } else {
-                throw('null data');
-        }
-        console.log(data);
-    };
-    
-    request.onerror = function() {
-        // There was a connection error of some sort
-    };
-    
-    request.send();
-    }
+// Add stylesheet
+var styles = document.createElement('link');
+	document.head.appendChild(styles);
+	styles.type = 'text/css';
+	styles.rel = 'index.css';
+
+// Add MathJax
+var MathJaxSC = document.createElement('script');
+	document.head.appendChild(MathJaxSC);
+	MathJaxSC.type = 'text/javascript';
+	MathJaxSC.src = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML';
+
+// Load pages.json
+var pages;
+	var xhr = new XMLHttpRequest();
+	xhr.open('get', 'http://eyamil.github.io/pages.json',true);
+	xhr.responseType = 'json';
+	xhr.onload = function() {
+		if (xhr.status = 200) {
+			pages = xhr.response;
+		}
+		else { // Needs to throw a more clear error
+			pages = null;
+		}
+	}
+	xhr.send();
