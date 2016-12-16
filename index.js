5f177f134434879b04e4151accec62b2fd8a4766
S L <sid.limaye11@gmail.com>
2016-12-15 20:41:19 -0500

    Updated pages and some Menu code now working

--- index.js
+++ index.js
@@ -13,13 +13,29 @@ var pages;
 	xhr.onload = function() {
 		if (xhr.status = 200) {
 			pages = xhr.response;
+			var menus = document.getElementsByClassName('menu');
+				Array.prototype.forEach.call(menus, makeMenu);
 		}
 		else { // Needs to throw a more clear error
 			pages = null;
 		}
 	}
+	function makeMenu(menuElement) {
+		var list = document.createElement('ul');
+			menuElement.appendChild(list);
+		var labels = Object.keys(pages.menu);
+		for (i = 0; i < labels.length; i++) {
+			var menuEl = document.createElement('li');
+				list.appendChild(menuEl);
+				var link = document.createElement('a');
+				link.href = pages.menu[labels[i]].url;
+				link.innerText = labels[i];
+				menuEl.appendChild(link);
+
+		}
+	}
 	xhr.send();
-	console.log(pages);
+
 
 // Add MathJax
 var MathJaxSC = document.createElement('script');
