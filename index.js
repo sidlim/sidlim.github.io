de44412f258882e0c8fbdef109f50d77fd279b43
S L <sid.limaye11@gmail.com>
2016-12-15 21:01:15 -0500

    Barebones theme completed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var pages;
 	xhr.onload = function() {
 		if (xhr.status = 200) {
 			pages = xhr.response;
-			var menus = document.getElementsByClassName('menu');
+			var menus = document.getElementsByClassName('menucontainer');
 				Array.prototype.forEach.call(menus, makeMenu);
 		}
 		else { // Needs to throw a more clear error
@@ -23,13 +23,16 @@ var pages;
 	function makeMenu(menuElement) {
 		var list = document.createElement('ul');
 			menuElement.appendChild(list);
+			list.className = 'menu';
 		var labels = Object.keys(pages.menu);
 		for (i = 0; i < labels.length; i++) {
 			var menuEl = document.createElement('li');
 				list.appendChild(menuEl);
+				menuEl.className = 'menuelement';
 				var link = document.createElement('a');
 				link.href = pages.menu[labels[i]].url;
 				link.innerText = labels[i];
+				link.className = 'menulink';
 				menuEl.appendChild(link);
 
 		}
