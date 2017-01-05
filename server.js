4c88c088d52dd724f16029c94c17733a61c80987
S L <sid.limaye11@gmail.com>
2017-01-05 11:24:01 -0500

    Server code finished

--- server.js
+++ server.js
@@ -1,27 +1,61 @@
 // Webserver Setup:
 var fs = require('fs');
 var http = require('http');
+var path = require('path')
 const PORT = process.env.PORT || 8080;
 
 function handleRequest(request, response){
 	console.log(request.url);
-	if (request.url.slice(-1) == '/') {
-		response.writeHead(200);
-	    var FaviconStream = fs.createReadStream(__dirname  + request.url + 'index.html');
-	    FaviconStream.pipe(response);
-		console.log('returned ' + __dirname);
+	var p = __dirname + request.url;
+	if ((request.url.substr(-1) == '/') && exists(p + '/index.html')) {
+		// Accessing a directory: Look for index.html
+		p += '/index.html';
+		fileret(p, response);
 	}
-	else if (fs.existsSync(__dirname + request.url)) {
-		response.writeHead(200);
-	    var FaviconStream = fs.createReadStream(__dirname  + request.url);
-	    FaviconStream.pipe(response);
-		console.log('returned ' + __dirname);
+	else if ( !(request.url.substr(-1) == '/') && exists(p)) {
+		// Accessing a file: Look for the file
+		fileret(p, response);
+	}
+	else if (!exists(p) && exists(__dirname + '/404.html')) {
+		// Hit a 404 with a prewritten 404 page
+		filret(404, response);
 	}
 	else {
-		console.log('not found');
+		// Regular old 404
+		console.log('404\'ed looking for ' + p);
+		response.writeHead(404, {"Content-Type": "text/plain"});
+		response.write("404 Not Found\n");
+		response.end();
 	}
 }
 
+// Existence checks, because fsexistssync is screwy
+function exists(dir) {
+	console.log('checking if ' + dir + ' exists.');
+	try {
+		fs.accessSync(dir);
+		return(true);
+	}
+	catch(e) {
+		return(false);
+	}
+}
+
+// Return a file:
+function fileret(path, resp) {
+	if (path == 404) {
+		resp.writeHead(404);
+		var FaviconStream = fs.createReadStream(__dirname + '404.html');
+	}
+	else {
+		resp.writeHead(200);
+		var FaviconStream = fs.createReadStream(path);
+	}
+	FaviconStream.pipe(resp);
+	console.log('returned ' + path);
+}
+
+// Run web server
 var WebServer = http.createServer(handleRequest);
 
 WebServer.listen(PORT, function(){
